edea50cbd6d0c45d442d00b51a31c96998b4c1c0
kockono <kockonoop@gmail.com>
2023-12-21 10:38:01 -0600

    🔧 chore(.gitignore): add .vscode/ to the list of ignored files and directories 🔧 chore(Ahk.py): change file mode from 100755 to 100644 for consistency ✨ feat(Tests): add new file Ahk.js for testing purposes ✨ feat(Tests): add new file hello_world_windows.cpp for testing purposes ✨ feat(Tests): add new file mac.cpp for testing purposes 🔥 chore(z.py): remove unused file z.py

--- Ahk.py
+++ Ahk.py
@@ -1,27 +1,85 @@
-import keyboard
+from pynput import keyboard
+# from pynput.keyboard import Key, Controller
 
-def on_key_event(e):
-    if e.event_type == keyboard.KEY_DOWN:
-        # Concatenar la tecla presionada al buffer
-        global key_buffer
-        key_buffer += e.name
+# Crea una instancia del controlador de teclado
+keyboard_controller = keyboard.Controller()
 
-        # Verificar si la combinación "h6" está presente en el buffer
-        if "h6" in key_buffer:
-            keyboard.write('######')
-            key_buffer = ""  # Limpiar el buffer después de imprimir
-            # exit()
+# Variables
+key_buffer = ""
 
-# Configurar la función de manejo de eventos para cualquier tecla
-keyboard.hook(on_key_event)
+# Diccionario de teclas
+keys = {
+    "h1": "#",
+    "h2": "##",
+    "h3": "###",
+    "h4": "####",
+    "h5": "#####",
+    "h6": "######",
+    "zz": ">",
+    "xx": "<"
+}
 
-# Mantener el programa en ejecución
-try:
-    print("El programa está escuchando. Presiona 'esc' para salir.")
-    keyboard.wait('shift + esc')
-except KeyboardInterrupt:
-    pass
-finally:
-    # Limpiar y detener la escucha del teclado
-    keyboard.unhook_all()
-    print("Programa detenido.")
+
+def on_press(key):
+    global key_buffer
+    try:
+        key_buffer += key.char
+        if key_buffer in keys:
+          for i in key_buffer:
+            keyboard_controller.press(keyboard.Key.backspace)
+          keyboard_controller.type(keys[key_buffer])
+          key_buffer = ""
+    except AttributeError:
+        key_buffer = ""
+        print('special key {0} pressed'.format(key))
+
+
+# def check_keys(key):
+#     global key_buffer
+#     if key == "h1":
+#       for i in key_buffer:
+#         keyboard_controller.press(keyboard.Key.backspace)
+#     key_buffer = ""
+
+
+
+# def reset_key_buffer_after_delay():
+#     global key_buffer
+#     threading.Timer(1.0, reset_key_buffer).start() 
+
+def reset_key_buffer():
+    key_buffer = ""
+    print(key_buffer)
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+
+
+
+# def on_activate_z():
+#     keyboard.Controller.type('>')
+
+# def on_activate_i():
+#     keyboard.Controller.type('<')
+
+# with keyboard.GlobalHotKeys({
+#         '<shift>+z': on_activate_z,
+#         '<alt>+z': on_activate_i}) as h:
+#     h.join()
