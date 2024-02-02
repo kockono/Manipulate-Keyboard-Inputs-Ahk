8e447e41c641699c2803a1768b5cf5d8affbd5df
kockono <kockonoop@gmail.com>
2024-02-02 14:00:22 -0600

    🚧 working(working): in special keys

--- Ahk.py
+++ Ahk.py
@@ -14,8 +14,23 @@ def on_press(key):
     try:
       if key.char is not None: # Si es un caracter agregalo al buffer
         key_buffer += key.char
+        if key_buffer.startswith('Ctrl+l'):
+          print('X')
+          reset_key_buffer()
         check_keys(key_buffer)
     except AttributeError:
+        if key == keyboard.Key.cmd_l:
+            print('Cmd pressed')
+            key_buffer += 'Ctrl+'
+        if key == keyboard.Key.ctrl_l:
+            print('Ctrl pressed')
+            key_buffer += 'Ctrl+'
+        elif key == keyboard.Key.alt_l:
+            print('Alt pressed')
+            key_buffer += 'Alt+'
+        elif key == keyboard.Key.shift:
+            print('Shift pressed')
+            key_buffer += 'Shift+'
         print('special key {0} pressed'.format(key))
         reset_key_buffer()
     else:
@@ -23,6 +38,8 @@ def on_press(key):
         #code to run if no error is raised
 
 def check_keys(key_buffer_final):
+    if key_buffer_final == 'Ctrl+T':
+      print("Realizando acción para Ctrl + T")
     for key, value in keys.items():
         if key_buffer_final.endswith(key):
             extracted_key = re.search(rf'{key}$', key_buffer_final).group()
@@ -32,11 +49,12 @@ def check_keys(key_buffer_final):
 
 def type_characters(key_buffer_final):
     text_to_type = keys[key_buffer_final]
+    # if check_caps_lock():
     for char in text_to_type:
         keyboard_controller.press(char)
         keyboard_controller.release(char)
         if char.isspace():
-           keyboard_controller.press(keyboard.Key.space)
+          keyboard_controller.press(keyboard.Key.space)
     reset_key_buffer_with_delay()
 
 def delete_characters(key_buffer_final):
@@ -58,6 +76,18 @@ def on_release(key):
     if key == keyboard.Key.f10:
         # Stop listener
         return False
+
+def check_caps_lock():
+    if keyboard_controller._controller.get_char() in keyboard_controller._controller.KEYBOARD_SPECIAL_KEYS:
+        # Obtén el estado de la tecla de bloqueo de mayúsculas (Caps Lock)
+        caps_lock_state = keyboard_controller._controller.get_char_state(keyboard_controller._controller.KEYBOARD_SPECIAL_KEYS['caps_lock'])
+        
+        if caps_lock_state:
+            print("Caps Lock está activado.")
+            return True
+        else:
+            print("Caps Lock está desactivado.")
+            return False
 
 # Collect events until released
 with keyboard.Listener(
--- keys_dict.py
+++ keys_dict.py
@@ -1,10 +1,14 @@
 keys = {
-    "h1": "# ",
-    "h2": "## ",
-    "h3": "### ",
-    "h4": "#### ",
-    "h5": "##### ",
-    "h6": "###### ",
-    "X": ">",
-    "Z": "<",
+    "h1": "#",
+    "h2": "##",
+    "h3": "###",
+    "h4": "####",
+    "h5": "#####",
+    "h6": "######",
+    "Z": ">",
+    "Ω": "<",
+    "exportpcm": "POSTGRESCONNECTIONSTRING='Host=10.19.5.100;Database=postgres;Username=postgres;Password=30083008;Pooling=false;Timeout=300;CommandTimeout=300'",
+    "exportraven": "RAVENDB_URL = \"http://10.19.5.41:8082/\"",
+    "makemigration":"dotnet ef migrations add \"AddTable\" --startup-project ../Delta.Api",
+    "updatemigration": "dotnet ef database update --startup-project ../Delta.api" 
 }
